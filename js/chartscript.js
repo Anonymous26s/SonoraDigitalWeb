badc1ba064fc521ffd847bc510cf0053587ecaac
Anonymous26s <Anonymous_26s@hotmail.com>
2019-10-11 01:05:14 -0700

    Add files via upload

---
+++ js/chartscript.js
@@ -0,0 +1,53 @@
+				
+Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: ''
+    },
+    subtitle: {
+        text: ''
+    },
+    xAxis: {
+        categories: [
+            'Photoshop',
+            'HTML + CSS',
+            'JavaScript',
+            'WordPress',
+            'Jquery',
+            'Diseño Responsive',
+            'Testing',
+            'FTP',
+            'SQL',
+            'PHP',
+            
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: ''
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.1,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Team',
+        data: [75, 90, 80, 85, 80, 90, 80, 85, 90, 70]
+
+    }]
+});
